3622b7dd306af480a271c36a57b413fdfe4fbb61
hamza <hamza.yazough1992@gmail.com>
2025-01-18 15:28:08 -0500

    init

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CommonModule } from '@angular/common';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { accessToken } from '../../assets/maps';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: accessToken,
+  }),
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    provideAuth0({
+      domain: 'dev-8cn4ee7fnjylxcsz.us.auth0.com',
+      clientId: 'RLU5dSYynQfFsVWfKtnoBmgpjqug8mEw',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }}), 
+    provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withInterceptorsFromDi())
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ front/src/app/pages/login-page/auth-button/auth-button.component.ts
@@ -0,0 +1,38 @@
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, filter, Subject, takeUntil } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html',
+  styleUrl: './auth-button.component.scss'
+})
+export class AuthButtonComponent implements OnInit, OnDestroy{
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    combineLatest([this.auth.isAuthenticated$])
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        filter(([isAuthenticated]) => isAuthenticated)
+      )
+      .subscribe();
+
+    this.auth.user$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe();
+
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+}
