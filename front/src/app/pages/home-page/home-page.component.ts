28c0a78e04b390d4fc1172e04b3dcd835f59c8fe
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2025-01-18 14:43:16 -0500

    add initial btns to homepage

--- front/src/app/pages/home-page/home-page.component.ts
+++ front/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  constructor(private router: Router) {}
 
+  navigate() {
+    this.router.navigate(['/navigate']); // Update with your navigation path
+  }
+
+  setupProfile() {
+    this.router.navigate(['/setup-profile']); // Update with your setup profile path
+  }
+
+  createEvent() {
+    this.router.navigate(['/create-event']); // Update with your create event path
+  }
 }
